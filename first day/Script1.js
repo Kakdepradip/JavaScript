6405527774c61e53c31c675575e164aedb23327c
Praddip kakde <kakdepradip21@gmail.com>
2025-12-27 19:45:09 +0530

    data types and scope with var,let,const

---
+++ first day/Script1.js
@@ -0,0 +1,36 @@
+/**console.log("welcome to my first progam");
+name2="pradip kakde"
+console.log(name2);
+//number=10;
+//console.log(number);
+ram=true;
+console.log(ram);
+float=1.500
+console.log(float);
+//Name="pradip kake ";
+//console.log(Name);
+funllName="pradip kakde";
+fullname="Praddip krushna kakde";
+console.log(fullname);
+console.log(funllName);
+Console="console";
+console.log(Console);
+//Scope in Javascript
+//let ,const,var
+//var:variable can not be redeclared and updated.A global scope variable
+//let:Variable can not be redeclared but can be updated.it is a block scope varibale.
+//const:variable can not be re-declared or updated.A block scope variable.1
+let name1="pradip";
+let number=14;
+let num=1.5;
+let age=15;
+ age=20;
+ age=50;
+console.log(age)
+let myname="pradip";
+
+ myname="pravin"
+console.log(myname);**/
+
+let fullname ="pradip kakdel";
+console.log(fullname);
