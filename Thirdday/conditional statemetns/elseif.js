0d8dcbc9cc5f0492a3520fbe401e8226d530d25f
Praddip kakde <kakdepradip21@gmail.com>
2025-12-28 00:05:15 +0530

    if else

---
+++ Thirdday/conditional statemetns/elseif.js
@@ -0,0 +1,24 @@
+/**let age=15;
+if(age<18){
+    console.log("you are a junior");
+    
+}else if(age>60){
+console.log("you are a oldage");
+
+}else if(age<80){
+    console.log("you are a geniuous");
+    
+}**/
+
+let mode="white";
+let color;
+if(mode==="dark"){
+    color="black"
+}else if(mode==="red"){
+    color="yelllow"
+}else if(mode==="white"){
+    color="pink"
+}else if(mode==="gold"){
+    color="shadow"
+}
+console.log(color)
